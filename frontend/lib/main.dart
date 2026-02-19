5bf95aa41eae26851fad143e2a3bb9b7c7a3d47b
iamsri884-hub <iamsri884@gmail.com>
2026-02-19 21:15:24 +0530

    Change home screen from Dashboard to Login

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/dashboard.dart';
+import 'screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Smart Dairy App',
-      home: const DashboardScreen(),
+      home: LoginScreen(),
     );
   }
 }
